bd809215e0da082fca4f7d2621fbbf07fc3dc386
reallym <yeming_1001@163.com>
2025-07-20 19:55:21 +0800

    初次提交

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,14 @@ export default function RootLayout({
         <link rel="canonical" href="https://lumadreammachine.com" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#06b6d4" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TTMLBQ237N"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-TTMLBQ237N');
+        `}} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
